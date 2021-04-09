108b1d54b29c60004ec40dc27a44045cfb3c42c6
Eric Peart <elpeart@gmail.com>
2021-04-08 21:33:13 -0400

    added saving files

--- filter_compare.m
+++ filter_compare.m
@@ -1,3 +1,4 @@
+pkg load signal % Octave only comment out for MATLAB
 wp = [0.3 0.5];
 ws = [0.2 0.6];
 rp = 1;
@@ -7,21 +8,25 @@ rs = 100;
 [b, a] = butter(n, Wn);
 freqz(b, a)
 title('Butter')
+saveas(gcf, 'Butter.jpg')
 
 [nc, Wnc] = cheb1ord(wp, ws, rp, rs);
 [bc, ac] = cheby1(nc, rp, Wnc);
 figure;
 freqz(bc, ac)
 title('cheby1')
+saveas(gcf, 'cheby1.jpg')
 
 [nc2, Wnc2] = cheb2ord(wp, ws, rp, rs);
 [bc2, ac2] = cheby1(nc2, rs, Wnc2);
 figure;
 freqz(bc2, ac2)
 title('cheby2')
+saveas(gcf, 'cheby2.jpg')
 
 [ne, Wne] = ellipord(wp, ws, rp, rs);
 [be, ae] = ellip(ne, rp, rs, Wne);
 figure;
 freqz(be, ae)
 title('ellipse')
+saveas(gcf, 'ellipse.jpg')
