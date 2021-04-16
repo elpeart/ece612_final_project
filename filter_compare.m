d6ffcec9faeb081cca56df46ac6a0a01f7d55b7c
Eric Peart <elpeart@gmail.com>
2021-04-15 22:07:43 -0400

    added error and continuous time plotting of results

--- filter_compare.m
+++ filter_compare.m
@@ -1,6 +1,6 @@
 pkg load signal % Octave only, comment out for Matlab
-wp = 40/100;
-ws = 60/100;
+wp = 90/100;
+ws = 95/100;
 rp = 1;
 rs = 100;
 t = 0:0.005:1;
@@ -12,7 +12,7 @@ figure;
 plot(t, sig)
 title('Original Signal')
 saveas(gcf, 'original.jpg')
-total = sig + noise;
+total = sig;% + noise;
 figure;
 plot(t, total)
 title('Signal plus Noise')
@@ -37,6 +37,12 @@ figure;
 stem(n1, yb)
 title('Butterworth Filtered Discrete')
 saveas(gcf, 'butter_filtered.jpg')
+but_err = mean(abs(sig - yb) / mean(abs(sig)));
+fprintf('Butterworth Filter Error: %4.4f\n', but_err);
+figure;
+plot(t, yb)
+title('Butterworth Continuous')
+saveas(gcf, 'but_cont.jpg')
 
 [nc, Wnc] = cheb1ord(wp, ws, rp, rs);
 [bc, ac] = cheby1(nc, rp, Wnc);
@@ -49,6 +55,12 @@ yc1 = filter(bc, ac, total_d);
 stem(n1, yc1)
 title('Cheby1 filtered')
 saveas(gcf, 'cheby1_filt.jpg')
+c1_err = mean(abs(sig - yc1) / mean(abs(sig)));
+fprintf('Chebyshev Type I Filter Error: %4.4f\n', c1_err);
+figure;
+plot(t, yc1)
+title('Chebyshev I continuous')
+saveas(gcf, 'cheby1_cont.jpg')
 
 [nc2, Wnc2] = cheb2ord(wp, ws, rp, rs);
 [bc2, ac2] = cheby1(nc2, rs, Wnc2);
@@ -61,6 +73,12 @@ yc2 = filter(bc2, ac2, total_d);
 stem(n1, yc2)
 title('Cheby2 filtered')
 saveas(gcf, 'cheby2_filt.jpg')
+c2_err = mean(abs(sig - yc2) / mean(abs(sig)));
+fprintf('Chebyshev Type II Filter Error: %4.4f\n', c2_err);
+figure;
+plot(t, yc2)
+title('Chebyshev II continuous')
+saveas(gcf, 'cheby2_cont.jpg')
 
 [ne, Wne] = ellipord(wp, ws, rp, rs);
 [be, ae] = ellip(ne, rp, rs, Wne);
@@ -70,6 +88,12 @@ title('ellipse')
 saveas(gcf, 'ellipse_response.jpg')
 figure;
 ye = filter(be, ae, total_d);
-stem(n1, yc1)
+stem(n1, ye)
 title('Ellipse filtered')
 saveas(gcf, 'ellipse_filt.jpg')
+e_err = mean(abs(sig - ye) / mean(abs(sig)));
+fprintf('Ellipse Filter Error: %4.4f\n', e_err);
+figure;
+plot(t, ye)
+title('Ellipse continuous')
+saveas(gcf, 'e_cont.jpg')
